60c3f62a15b5c04eb9dbafb8a79b9e156a673bef
sarahneely <sporter1990@hotmail.com>
2017-10-19 18:14:57 -0700

    started adding the array of books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,27 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'app';
+  public query: string;
+  public queryString: string;
+  public bookList;
+  title = "bookList";
 }
+
+ngOnInit () {
+
+}
+
+Array<books> = [
+  {title: 'His Dark Materials',
+  author: 'Phillip Pulman',
+  published: '1995-2000',
+  thumbnail: url(''),
+  notes: "His Dark Materials is a trilogy consisting of The Golden Compass, The Subtle Knife, and The Ember Spyglass. The Golden Compass was made into a film in 2007 starring Nicole Kidman and Daniel Craig."
+  }
+  {title: '1984',
+  author: 'George Orwell',
+  published: 'June 8th, 1949',
+  thumbnail: url(''),
+  notes: "1984 is a story about a dystopian society. It was written in 1949 and ended up being quite prophetic."
+  }
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
